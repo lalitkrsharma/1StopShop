bb5c68630e683379fe68eaffad1ccc23210a619a
Lalit Kumar Sharma <lalitkumarsharma.jsr@gmail.com>
2022-11-14 22:28:30 +0530

    Sanity Setup

--- sanity_studio/schemas/product.js
+++ sanity_studio/schemas/product.js
@@ -1,4 +1,4 @@
-export default{
+export default {
   name: 'product',
   title: 'Product',
   type: 'document',
@@ -7,31 +7,31 @@ export default{
       name: 'image',
       title: 'Image',
       type: 'array',
-      of: [{type: 'image'}],
+      of: [{ type: 'image' }],
       options: {
-        hotspot:true,
+        hotspot: true,
       }
     },
-    {
+    { 
       name: 'name',
       title: 'Name',
       type: 'string',
     },
-    {
+    { 
       name: 'slug',
       title: 'Slug',
       type: 'slug',
       options: {
         source: 'name',
-        maxlength: 90,
+        maxLength: 90,
       }
     },
-    {
+    { 
       name: 'price',
       title: 'Price',
       type: 'number',
     },
-    {
+    { 
       name: 'details',
       title: 'Details',
       type: 'string',
